a2b6d8597a63daaf74e7022fc89c2a82f0475145
CLown1331 <clown1331@gmail.com>
2020-01-14 19:53:50 +0600

    check if solved

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const options = yargs
     .usage("Usage: -u <username>")
     .usage("Usage: -p <password>")
     .usage("Usage: -i <problemId>")
-    .option("f", { alias: "file", describe: "File Name", type: "string", demandOption: true })
+    .option("f", { alias: "file", describe: "File Name", type: "string", demandOption: false })
     .option("u", { alias: "username", describe: "Username", type: "string", demandOption: true })
     .option("p", { alias: "password", describe: "Password", type: "string", demandOption: true })
     .option("i", { alias: "problemId", describe: "ProblemId", type: "string", demandOption: true })
@@ -20,6 +20,10 @@ const pwd = options.path || process.env.PWD;
 const path = pwd + "/" + options.file;
 const fileContent = readContent(path);
 
-lightoj.submit(options.username, options.password, fileContent, options.problemId).then(r => {
+lightoj.checkIfSolved(options.username, options.password, options.problemId).then(r => {
     console.log(r);
 });
+
+// lightoj.submit(options.username, options.password, fileContent, options.problemId).then(r => {
+//     console.log(r);
+// });
--- lightoj.js
+++ lightoj.js
@@ -1,8 +1,10 @@
 const getBrowser = require('./browser').getBrowser;
+const cheerio = require('cheerio');
 
 const submit = async(username, password, file, problemId) => {
+    let browser;
     try {
-        const browser = await getBrowser();
+        browser = await getBrowser();
         const page = await browser.newPage();
         await page.goto('http://lightoj.com/login_main.php');
         await page.type('#myuserid', username);
@@ -19,9 +21,46 @@ const submit = async(username, password, file, problemId) => {
     } catch(err) {
         console.error(err);
         return 'Fail';
+    } finally {
+        browser.close();
+    }
+}
+
+const checkIfSolved = async(username, password, problemId) => {
+    let browser;
+    try {
+        browser = await getBrowser();
+        const page = await browser.newPage();
+        await page.goto('http://lightoj.com/login_main.php');
+        await page.type('#myuserid', username);
+        await page.type('#mypassword', password);
+        await page.click('input[type="submit"]');
+        await page.waitForNavigation();
+
+        await page.goto(`http://lightoj.com/volume_userstat.php`, {
+          waitUntil: 'networkidle2',
+        });
+    
+        const html = await page.content();
+        
+        const $ = cheerio.load(html);
+
+        const solved = $('.leftTop').eq(3).parent().parent().find('a').map(function () {
+          return $(this).text().trim();
+        }).toArray();
+
+        const isSolved = !!solved.find(id => id === problemId);
+
+        return `loj-${problemId}: ${isSolved}`;
+    } catch(err) {
+        console.error(err);
+        return 'Fail';
+    } finally {
+        browser.close();
     }
 }
 
 module.exports = {
-    submit
+    submit,
+    checkIfSolved
 };
